2eae49704ae13cbfb5f3427b21e2a30bb68cd93b
tangping <1959152133@qq.com>
2022-11-11 18:01:17 +0800

    init

---
+++ index.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "extends": "stylelint-config-standard",
+  "rules": {
+    "alpha-value-notation": "number",
+    "selector-class-pattern": ".*",
+    "font-family-no-missing-generic-family-keyword": null,
+    "selector-type-no-unknown": null,
+    "block-closing-brace-empty-line-before": null,
+    "no-duplicate-selectors": null,
+    "font-family-no-duplicate-names": null,
+    "at-rule-no-unknown": null
+  },
+  "files": ["vue", "css", "less"]
+}
